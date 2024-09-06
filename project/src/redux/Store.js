fce221bd6ceb3bfad03f46027e9ca8a4c251b187
Sakshi Suman <126015310+coderrsuman@users.noreply.github.com>
2024-09-06 22:43:20 +0530

    Delete project directory

--- project/src/redux/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import CartSlice from "./slices/CartSlice";
-
-export const store = configureStore({
-    reducer: {
-        cart: CartSlice
-    }
-})
